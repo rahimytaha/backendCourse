ea528916821075bb111decc9a42a9fd6d674a8b4
KianJanloo <KianJanloo10@gmail.com>
2026-04-28 00:00:13 +0330

    docs: news

--- 01/services/news/index.service.js
+++ 01/services/news/index.service.js
@@ -1,21 +1,26 @@
 const { toRoleNames } = require("../../utils/auth.util");
 const prisma = require("../../utils/client.util");
 
+// creating news with data
 const createNews = async (data, author_id) => {
   const news = await prisma.news.create({ data: { ...data, author_id } });
   return news;
 };
 
+// get all news with filters
 const getAllNews = async (title, content, author_id, published, order) => {
   let where = {};
 
+  // search as title
   if (title) where.title = { contains: title, mode: "insensitive" };
+  // search as content
   if (content) where.content = { contains: content, mode: "insensitive" };
-
+  // filter by published
   if (published !== undefined) {
     where.published = published === "true" || published === true;
   }
 
+  // check author and implement filter system for it
   if (author_id !== undefined) {
     const author = await prisma.user.findUnique({
       where: { id: author_id },
@@ -28,22 +33,29 @@ const getAllNews = async (title, content, author_id, published, order) => {
     where.author_id = author_id;
   }
 
+  // return list of news
   return prisma.news.findMany({
     where,
+    // order filter
     orderBy: { createdAt: order === "asc" ? "asc" : "desc" },
   });
 };
 
+// get a news by id
 const getNewsById = async (id) => {
+  // check news
   const news = await prisma.news.findUnique({ where: { id } });
   if (!news) throw new Error("news not found");
   return news;
 };
 
+// update a news with data and id
 const updateNews = async (id, data, userId) => {
+  // check news
   const news = await prisma.news.findUnique({ where: { id } });
   if (!news) throw new Error("news not found");
 
+  // check user and role of user
   const user = await prisma.user.findUnique({ where: { id: userId } });
   const userRoles = toRoleNames(user.userRoles)
   if (
@@ -57,10 +69,13 @@ const updateNews = async (id, data, userId) => {
   return prisma.news.update({ where: { id }, data });
 };
 
+// delete a news by id
 const deleteNews = async (id, userId) => {
+  // check news
   const news = await prisma.news.findUnique({ where: { id } });
   if (!news) throw new Error("news not found");
 
+  // check user and role of user
   const user = await prisma.user.findUnique({ where: { id: userId } });
   const userRoles = toRoleNames(user.userRoles)
   if (
@@ -74,10 +89,13 @@ const deleteNews = async (id, userId) => {
   return prisma.news.delete({ where: { id } });
 };
 
+// publish or unpublish news
 const publishNews = async (id, published, userId) => {
+  // check news
   const news = await prisma.news.findUnique({ where: { id } });
   if (!news) throw new Error("news not found");
 
+  // check user and role of user
   const user = await prisma.user.findUnique({ where: { id: userId } });
   const userRoles = toRoleNames(user.userRoles)
   if (
