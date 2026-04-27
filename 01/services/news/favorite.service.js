0a2035388d91556ffc002ffffc1d781d6bf794ab
KianJanloo <KianJanloo10@gmail.com>
2026-04-28 00:07:00 +0330

    docs: news favorite service

--- 01/services/news/favorite.service.js
+++ 01/services/news/favorite.service.js
@@ -1,13 +1,17 @@
 const prisma = require("../../utils/client.util");
 
+// toggle favorite (news)
 const toggleFavoriteNews = async (news_id, user_id) => {
+  // check news
   const news = await prisma.news.findUnique({ where: { id: news_id } });
   if (!news) throw new Error("News not found");
 
+  // check exisitng favortie (if the user marked this news as his favorites or not)
   const existing = await prisma.news_favorite.findFirst({
     where: { news_id, user_id },
   });
 
+  // if it has marked as users favorites: delete the mark
   if (existing) {
     await prisma.news_favorite.delete({
       where: { id: existing.id },
@@ -16,6 +20,7 @@ const toggleFavoriteNews = async (news_id, user_id) => {
     return { message: "Removed from favorites" };
   }
 
+  // if it hasn't marked as users favorites: mark as favorite
   await prisma.news_favorite.create({
     data: { news_id, user_id },
   });
@@ -23,6 +28,7 @@ const toggleFavoriteNews = async (news_id, user_id) => {
   return { message: "Added to favorites" };
 };
 
+// get favorites list
 const getUserFavorites = async (user_id) => {
   return prisma.news_favorite.findMany({
     where: { user_id },
